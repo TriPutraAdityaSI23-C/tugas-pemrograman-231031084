9c41a0385888af9620a111469b122bb034fe5682
Nathz <triputraadityaaditya@gmail.com>
2023-12-23 14:35:49 +0700

    Upload File Tugas9

---
+++ 231031084-adit-tugas-9-c.py
@@ -0,0 +1,17 @@
+vd = {"nama" : "Tri Putra Aditya",
+      "kelas" : "SI23-C",
+      "nim" : "231031084",
+      "asal" : "Bone",
+      "tanggal" : "22-12-2004",
+      "alamat" : "Jl. Kelapa Gading",
+      "hobi" : "Main game",
+      "tinggi" : "163 cm",
+      }
+print("Nama Lengkap  :",vd["nama"])
+print("Kelas         :",vd["kelas"])
+print("NIM           :",vd["nim"])
+print("Tmpt tgl lahir:",vd["asal"],vd["tanggal"])
+print("Alamat        :",vd["alamat"])
+print("Hobi          :",vd["hobi"])
+print("Tinggi        :",vd["tinggi"])
+print("\nTipe data var vd : ",type(vd))
